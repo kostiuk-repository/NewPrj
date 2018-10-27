a750b1221f10d102f234a86c85579eeb1c4ab3bc
KostiukStudent <kostiukvladyslav@gmail.com>
2018-10-27 15:03:56 +0300

    Message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,7 +23,7 @@ public class Main {
         Project project3 = new Project("some_project-3",
             LocalDate.of(2019,9,15).atStartOfDay(ZoneId.of("Europe/Paris")).toInstant(),
             LocalDate.of(2020,11,15).atStartOfDay(ZoneId.of("Europe/Paris")).toInstant());
-        
+
         project1.setUuid(UUID.randomUUID().toString());
         project2.setUuid(UUID.randomUUID().toString());
         project3.setUuid(UUID.randomUUID().toString());
